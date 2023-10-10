c94fc1e210579b3c4e03bfe62b28c60764ade9ca
GitHub Action <action@github.com>
2023-10-10 11:22:29 +0000

    (skip): 🔖 bump version to 0.9.6-dev.73

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -28,6 +28,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.4.38.56")]
-[assembly: AssemblyFileVersion("0.4.38.56")]
-[assembly: AssemblyInformationalVersion("0.4.38-dev.56+Branch.dev.Sha.7a59cff23d6a688ccb50206e421f03bcaf40cd7b")]
+[assembly: AssemblyVersion("0.9.6.73")]
+[assembly: AssemblyFileVersion("0.9.6.73")]
+[assembly: AssemblyInformationalVersion("0.9.6-dev.73+Branch.dev.Sha.2a5f671a63e11a5cb14199214421846cde465fe8")]
