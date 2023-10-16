38fc741316471f9e8fe31bf662e47d3153f6b7c4
GitHub Action <action@github.com>
2023-10-16 13:12:00 +0000

    (skip): 🔖 bump version to 0.13.1-dev.128

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -28,6 +28,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.11.7.120")]
-[assembly: AssemblyFileVersion("0.11.7.120")]
-[assembly: AssemblyInformationalVersion("0.11.7-dev.120+Branch.dev.Sha.8b9663b2919d53b31783ebb021c4c9d91265cd26")]
+[assembly: AssemblyVersion("0.13.1.128")]
+[assembly: AssemblyFileVersion("0.13.1.128")]
+[assembly: AssemblyInformationalVersion("0.13.1-dev.128+Branch.dev.Sha.bd42c94f63de7e1b9a6e96dda91b3731cc1cc3de")]
