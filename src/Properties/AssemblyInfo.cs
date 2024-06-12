a7555083fb3fd06c6f540f3778fe11856786fcdf
GitHub Action <action@github.com>
2024-06-12 15:39:06 +0000

    (skip): 🔖 bump version to 0.15.6-dev.178

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -28,6 +28,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.15.6.176")]
-[assembly: AssemblyFileVersion("0.15.6.176")]
-[assembly: AssemblyInformationalVersion("0.15.6-dev.176+Branch.dev.Sha.e340cb86efb9b33d6b607443f56e772193f8bc55")]
+[assembly: AssemblyVersion("0.15.6.178")]
+[assembly: AssemblyFileVersion("0.15.6.178")]
+[assembly: AssemblyInformationalVersion("0.15.6-dev.178+Branch.dev.Sha.79767f2c816c42c34f1340b584b8d8f300d3f597")]
